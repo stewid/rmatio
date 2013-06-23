449024286035498dfbd946e3a177bb02e9823c77
Stefan Widgren <stefan.widgren@sva.se>
2013-06-23 14:58:40 +0200

    Added test for writing a character vector.
    * Added on.exit to each test that writes to file,
      to unlink file.

--- inst/tests/test_rmatio.r
+++ inst/tests/test_rmatio.r
@@ -29,6 +29,7 @@ context("dgCMatrix")
 
 test_that("dgCMatrix: case-1", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
   
   a.1 <- Matrix(c(0, 0, 0, 0, 0, 0, 1, 0, 0,
                   0, 0, 0, 0, 0, 0, 0, 1, 0,
@@ -42,12 +43,11 @@ test_that("dgCMatrix: case-1", {
   a.2 <- read.mat(filename)[['a']]
 
   expect_identical(a.2, a.1)
-
-  unlink(filename)
 })
 
 test_that("dgCMatrix: case-2", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   a.1 <- as(diag(1:5), 'dgCMatrix')
 
@@ -55,75 +55,69 @@ test_that("dgCMatrix: case-2", {
   a.2 <- read.mat(filename)[['a']]
 
   expect_identical(a.2, a.1)
-
-  unlink(filename)
 })
 
 context("vector")
 
 test_that("vector: case-1", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   write.mat(list(a=1:5), filename=filename)
   a <- read.mat(filename)[['a']]
   storage.mode(a) <- 'integer'
 
   expect_identical(a, 1:5)
-  
-  unlink(filename)
 })
 
 test_that("vector: case-2", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   write.mat(list(a=c(1,2,3,4,5)), filename=filename)
   a <- read.mat(filename)[['a']]
 
   expect_identical(a, c(1,2,3,4,5))
-  
-  unlink(filename)
 })
 
 test_that("vector: case-3", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   write.mat(list(a=1), filename=filename)
   a <- read.mat(filename)[['a']]
 
   expect_identical(a, 1)
-  
-  unlink(filename)
 })
 
 context("matrix")
 
 test_that("matrix: case-1", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   write.mat(list(a=matrix(1:9, nrow=3)), filename=filename)
   a <- read.mat(filename)[['a']]
   storage.mode(a) <- 'integer'
 
   expect_identical(a, matrix(1:9, nrow=3))
-  
-  unlink(filename)
 })
 
 test_that("matrix: case-2", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   write.mat(list(a=matrix(c(1,2,3,4,5,6,7,8,9), nrow=3)), filename=filename)
   a <- read.mat(filename)[['a']]
 
   expect_identical(a, matrix(c(1,2,3,4,5,6,7,8,9), nrow=3))
-  
-  unlink(filename)
 })
 
 context("array")
 
 test_that("array: case-1", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   a.1 <- array(seq_len(32^3), c(32,32,32))
 
@@ -132,12 +126,11 @@ test_that("array: case-1", {
   storage.mode(a.2) <- 'integer'
 
   expect_identical(a.2, a.1)
-  
-  unlink(filename)
 })
 
 test_that("array: case-2", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   a.1 <- array(seq_len(32^3), c(32,32,32))
   storage.mode(a.1) <- 'double'
@@ -146,14 +139,13 @@ test_that("array: case-2", {
   a.2 <- read.mat(filename)[['a']]
 
   expect_identical(a.2, a.1)
-  
-  unlink(filename)
 })
 
 context("complex")
 
 test_that("complex: case-1", {
   filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
 
   a.1 <- array(complex(real=1:20,imaginary=21:40), c(4,5))
 
@@ -161,7 +153,21 @@ test_that("complex: case-1", {
   a.2 <- read.mat(filename)[['a']]
 
   expect_identical(a.2, a.1)
-  
-  unlink(filename)
 })
 
+context("string")
+
+test_that("string: case-1", {
+  filename <- tempfile(fileext = ".mat")
+  on.exit(unlink(filename))
+    
+  a.1 <- c("abcdefghijklmnopqrstuvwxyz",
+           "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+           "1234567890!@#$%^&*()-_=+`~",
+           "[{]}\\|;:'\",<.>/?          ")
+
+  write.mat(list(a=a.1), filename=filename)
+  a.2 <- read.mat(filename)[['a']]
+    
+  expect_identical(a.2, a.1)
+})
