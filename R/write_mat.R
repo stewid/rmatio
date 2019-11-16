ab87dadac29d874bac6b259e2283be7772bddf62
Stefan Widgren <stefan.widgren@gmail.com>
2019-11-16 09:58:59 +0100

    Code style

--- R/write_mat.R
+++ R/write_mat.R
@@ -22,17 +22,17 @@
 ##' \itemize{
 ##'   \item A vector is saved as a \code{1 x length} array
 ##'
-##'   \item Support for writing a sparse matrix of type 'dgCMatrix' or 'lgCMatrix'
-##'     to file
-##' }
+##'   \item Support for writing a sparse matrix of type 'dgCMatrix' or
+##'     'lgCMatrix' to file }
 ##' @rdname write.mat-methods
 ##' @docType methods
 ##' @title Write Matlab file
 ##' @param object The \code{object} to write.
 ##' @param filename The MAT file to write.
-##' @param compression Use compression when writing variables. Defaults to TRUE.
+##' @param compression Use compression when writing
+##'     variables. Defaults to TRUE.
 ##' @param version MAT file version to create. Currently only support
-##' for Matlab level-5 file (MAT5) from rmatio package.
+##'     for Matlab level-5 file (MAT5) from rmatio package.
 ##' @return invisible NULL
 ##' @keywords methods
 ##' @include have_lib.R
@@ -43,7 +43,7 @@
 ##' filename <- tempfile(fileext = ".mat")
 ##'
 ##' ## Example how to read and write an integer vector with rmatio
-##' write.mat(list(a=1:5), filename=filename)
+##' write.mat(list(a = 1:5), filename = filename)
 ##' a <- as.integer(read.mat(filename)[["a"]])
 ##'
 ##' stopifnot(identical(a, 1:5))
@@ -52,16 +52,19 @@
 ##'
 ##' ## Read a compressed version 5 MAT file
 ##' m <- read.mat(system.file("extdata/matio_test_cases_compressed_le.mat",
-##'                           package='rmatio'))
+##'                           package = "rmatio"))
 ##'
 ##' ## Write an uncompressed version 5 MAT file
-##' write.mat(m, filename="test-uncompressed.mat", compression=FALSE, version="MAT5")
+##' write.mat(m, filename = "test-uncompressed.mat",
+##'           compression = FALSE, version = "MAT5")
 ##'
 ##' ## Write a compressed version 5 MAT file
-##' write.mat(m, filename="test-compressed.mat", compression=TRUE, version="MAT5")
+##' write.mat(m, filename = "test-compressed.mat",
+##'           compression = TRUE, version = "MAT5")
 ##'
 ##' ## Check that the content of the files are identical
-##' identical(read.mat("test-uncompressed.mat"), read.mat("test-compressed.mat"))
+##' identical(read.mat("test-uncompressed.mat"),
+##'           read.mat("test-compressed.mat"))
 ##'
 ##' unlink("test-uncompressed.mat")
 ##' unlink("test-compressed.mat")
@@ -75,27 +78,25 @@
 ##'                         d = "numeric"))
 ##'
 ##' ## Create a function to coerce a 'DemoS4Mat' object to a list.
-##' setAs(from="DemoS4Mat",
-##'       to="list",
-##'       def=function(from)
-##'       {
-##'         return(list(a=from@@a,
-##'                     b=from@@b,
-##'                     c=from@@c,
-##'                     d=from@@d))
+##' setAs(from = "DemoS4Mat",
+##'       to = "list",
+##'       def = function(from) {
+##'         return(list(a = from@@a,
+##'                     b = from@@b,
+##'                     c = from@@c,
+##'                     d = from@@d))
 ##'       }
 ##' )
 ##'
 ##' ## Create a function to coerce a list to a 'DemoS4Mat' object.
-##' setAs(from="list",
-##'       to="DemoS4Mat",
-##'       def=function(from)
-##'       {
+##' setAs(from = "list",
+##'       to = "DemoS4Mat",
+##'       def = function(from) {
 ##'         return(new("DemoS4Mat",
-##'                     a=from[["a"]],
-##'                     b=as.integer(from[["b"]]),
-##'                     c=from[["c"]],
-##'                     d=from[["d"]]))
+##'                     a = from[["a"]],
+##'                     b = as.integer(from[["b"]]),
+##'                     c = from[["c"]],
+##'                     d = from[["d"]]))
 ##'       }
 ##' )
 ##'
@@ -105,8 +106,7 @@
 ##'           function(object,
 ##'                    filename,
 ##'                    compression,
-##'                    version)
-##'           {
+##'                    version) {
 ##'             ## Coerce the 'DemoS4Mat' object to a list and
 ##'             ## call 'rmatio' 'write.mat' with the list.
 ##'             return(write.mat(as(object, "list"),
@@ -121,12 +121,12 @@
 ##'                  a = Matrix(c(0, 0, 0, 0, 0, 0, 1, 0, 0,
 ##'                               0, 0, 0, 0, 0, 0, 0, 1, 0,
 ##'                               0, 0, 0, 0, 0, 0, 0, 0, 1),
-##'                               nrow=3,
-##'                               ncol=9,
-##'                               byrow=TRUE,
-##'                               sparse=TRUE),
+##'                               nrow = 3,
+##'                               ncol = 9,
+##'                               byrow = TRUE,
+##'                               sparse = TRUE),
 ##'                  b = 1:5,
-##'                  c = matrix(as.numeric(1:9), nrow=3),
+##'                  c = matrix(as.numeric(1:9), nrow = 3),
 ##'                  d = c(6.0, 7.0, 8.0))
 ##'
 ##' ## Write to MAT file
@@ -154,52 +154,54 @@ setMethod("write.mat",
           function(object,
                    filename,
                    compression,
-                   version)
-          {
-            ## Check filename
-            if (any(!is.character(filename),
-                    !identical(length(filename), 1L),
-                    nchar(filename) < 1)) {
-              stop("'filename' must be a character vector of length one")
-            }
+                   version) {
+              ## Check filename
+              if (any(!is.character(filename),
+                      !identical(length(filename), 1L),
+                      nchar(filename) < 1)) {
+                  stop("'filename' must be a character vector of length one")
+              }
 
-            ## Check compression
-            if (any(!is.logical(compression),
-                    !identical(length(compression), 1L))) {
-              stop("'compression' must be a logical vector of length one")
-            }
+              ## Check compression
+              if (any(!is.logical(compression),
+                      !identical(length(compression), 1L))) {
+                  stop("'compression' must be a logical vector of length one")
+              }
 
-            if (identical(compression, TRUE)) {
-                if (!have.zlib()) {
-                    stop(paste("Sorry, library 'zlib' is not available.",
-                               "Use 'compression=FALSE' or install with 'zlib'"))
-                }
-                compression = 1L
-            } else {
-                compression = 0L
-            }
+              if (identical(compression, TRUE)) {
+                  if (!have.zlib()) {
+                      stop(paste("Sorry, library 'zlib' is not available.",
+                                 "Use 'compression = FALSE' or",
+                                 "install with 'zlib'"))
+                  }
+                  compression = 1L
+              } else {
+                  compression = 0L
+              }
 
-            ## Check version
-            version <- match.arg(version)
-            if (identical(version, "MAT5")) {
-              version <- 0x0100L
-              header <- sprintf("MATLAB 5.0 MAT-file, Platform: %s, Created By: rmatio v%s on %s",
-                                R.version$platform[[1]],
-                                utils::packageVersion('rmatio'),
-                                date())
-            } else {
-              stop("Unsupported version")
-            }
+              ## Check version
+              version <- match.arg(version)
+              if (identical(version, "MAT5")) {
+                  version <- 0x0100L
+                  header <- sprintf(paste0("MATLAB 5.0 MAT-file, ",
+                                           "Platform: %s, ",
+                                           "Created By: rmatio v%s on %s"),
+                                    R.version$platform[[1]],
+                                    utils::packageVersion('rmatio'),
+                                    date())
+              } else {
+                  stop("Unsupported version")
+              }
 
-            ## Check names in object
-            if (any(is.null(names(object)),
-                    !all(nchar(names(object))),
-                    any(duplicated(names(object))))) {
-              stop("All values in the list must have a unique name")
-            }
+              ## Check names in object
+              if (any(is.null(names(object)),
+                      !all(nchar(names(object))),
+                      any(duplicated(names(object))))) {
+                  stop("All values in the list must have a unique name")
+              }
 
-            .Call(write_mat, object, filename, compression, version, header)
+              .Call(write_mat, object, filename, compression, version, header)
 
-            invisible(NULL)
+              invisible(NULL)
           }
 )
